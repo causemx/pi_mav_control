158fcfe119ca56d65859ca3573b32959440ec928
Nanji Owl <allen.cause@gmail.com>
2023-02-22 17:47:49 +0800

    Clean repo and wirte test.

--- control.py
+++ control.py
@@ -3,17 +3,27 @@ import argparse
 import time
 
 
-def connection(host, port, retry=3):
+def connect(args, retry=3):
+    global master
+    _args = args.connect_info
     _retry = 0
     while True:
         try:
-            master = mavutil.mavlink_connection('/dev/serial0', baud=921600)
-            # master = mavutil.mavlink_connection('udpin:{}:{}'.format(host, port))
+            #master = mavutil.mavlink_connection('/dev/serial0', baud=921600)
+            #master = mavutil.mavlink_connection('udpin:{}:{}'.format(host, port))
+            if _args[0] == "serial":
+                master = mavutil.mavlink_connection(_args[1], baud=int(_args[2]))
+            elif _args[0] == "udp":
+                master = mavutil.mavlink_connection('udpin:{}:{}'.format(_args[1], _args[2]))
+            else:
+                print("Unknown connection type.")
+                break
+
             master.wait_heartbeat()
             print('connected')
-            return master
+            break
         except:
-            print('retry')
+            print("retry...:{}".format(_retry))
             _retry = _retry + 1
             if _retry > retry:
                 print('Can not connected to uav.')
@@ -23,6 +33,11 @@ def connection(host, port, retry=3):
 def main(args) -> int:
     parser = argparse.ArgumentParser()
     subparser = parser.add_subparsers()
+
+    parser_connect = subparser.add_parser("connect", help="Connect to UAV.")
+    parser_connect.add_argument("connect_info", nargs='+', help="For serial port connect, input: 'serial' [device] [baudrate]; \
+        For UDP input connect, input: 'udp' [host] [port].") 
+    parser_connect.set_defaults(func=connect)
     
     parser_arm = subparser.add_parser("arm", help="Arming/Disarm UAV throttle.")
     parser_arm.add_argument('--isarm', type=int, default=1, help="1: arm, 0: disarm.")
@@ -50,7 +65,7 @@ def main(args) -> int:
         pass
     return 0
 
-def arm(args):
+def arm(args) -> None:
     master.mav.command_long_send(master.target_system, master.target_component, \
         mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, args.isarm, 0, 0, 0, 0, 0, 0)
 
@@ -60,24 +75,25 @@ def arm(args):
         break
 
 
-def takeoff(args):
+def takeoff(args) -> None:
     master.mav.command_long_send(master.target_system, master.target_component, \
         mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, args.height)
 
     while True:
         # msg = master.recv_match(type='STATUSTEXT', blocking=True)
-        msg = master.recv_match(type='COMMAND_ACK', blocking=True)
+        msg = master.recv_match(type='COMMAND_ACK', blocking=True, timeout=1)
         print(msg)
         break
 
-def mode(args):
+
+def mode(args) -> None:
     while True:
         msg = master.recv_match(type='HEARTBEAT', blocking = True)
         mode = mavutil.mode_string_v10(msg)
         print(mode)
         break
 
-def set_mode(args):
+def set_mode(args) -> None:
     if args.mode not in master.mode_mapping():
         print('Unknown mode : {}'.format(args.mode))
         print('Try:', list(master.mode_mapping().keys()))
@@ -107,10 +123,10 @@ def set_mode(args):
         print(mavutil.mavlink.enums['MAV_RESULT'][ack_msg['result']].description)
         break
 
-def move(args):
+def move(args) -> None:
     master.mav.send(mavutil.mavlink.MAVLink_set_position_target_local_ned_message(10, master.target_system, \
         master.target_component, mavutil.mavlink.MAV_FRAME_LOCAL_NED, \
-            int(0b010111111000), args.movement, 0, -10, 0, 0, 0, 0, 0, 0, 0, 0))
+            int(0b010111111000), 0, args.movement, -10, 0, 0, 0, 0, 0, 0, 0, 0))
     
     while True:
         msg = master.recv_match(
@@ -120,11 +136,6 @@ def move(args):
 
 
 if __name__ == "__main__":
-    try:
-        master = connection("127.0.0.1", 14551)
-    except ConnectionError as err:
-        exit(err.errno)
-    
     while True:
         msg = input(" -> ")
         error_code = main(msg.split())

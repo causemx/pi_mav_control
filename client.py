55f9246324a1e5d17969deadb39fc5085b9fbfd4
Nanji Owl <allen.cause@gmail.com>
2023-03-06 11:06:15 +0800

    Seperate arg from control.

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
-import select, sys, argparse
+import select
+import sys
+import arg_parse
 from socket import socket, AF_INET, SOCK_STREAM
 from ctl import control
 
@@ -42,35 +44,9 @@ class Client:
     
     def handle_recv(self, recv):
         print('received: {}'.format(recv))
-        args = recv.split()
-
-        parser = argparse.ArgumentParser()
-        subparser = parser.add_subparsers()
-       
-        parser_arm = subparser.add_parser("arm", help="Arming/Disarm UAV throttle.")
-        parser_arm.add_argument('--isarm', type=int, default=1, help="1: arm, 0: disarm.")
-        parser_arm.set_defaults(func=control.arm)
-
-        parser_takeoff = subparser.add_parser("takeoff", help="Takeoff UAV to expect height.")
-        parser_takeoff.add_argument('height', type=int, default=2, help="Expect height for UAV takeoff.")
-        parser_takeoff.set_defaults(func=control.takeoff)
-
-        parser_mode = subparser.add_parser("mode", help="Get current UAV mode.")
-        parser_mode.set_defaults(func=control.mode)
-
-        parser_setmode = subparser.add_parser("setmode", help="Set UAV mode.")
-        parser_setmode.add_argument("mode", type=str, help="Enter expected mode here.")
-        parser_setmode.set_defaults(func=control.set_mode)
-        
-        parser_move = subparser.add_parser("move", help="Make UAV moving [East, North, Down] in local coord.")
-        parser_move.add_argument("movement", type=int, nargs='+', help="Move [east/west] [north/south] [down/up] for meters")
-        parser_move.add_argument("-e", "--east", type=int, default=0, help="Move east/west for n meters.")
-        parser_move.add_argument("-n", "--north", type=int, default=0, help="Move north/south for n meters.")
-        parser_move.add_argument("-d", "--down", type=int, default=0, help="Move down/Up for n meters.")
-        parser_move.set_defaults(func=control.move)
 
         try:
-            args = parser.parse_args(args)
+            args = arg_parse.parse(recv.split())
             args.func(args)
         except:
             pass
